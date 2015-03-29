09fc7af16c7249c3c619177aefb63b3133ee0fae
Kenji Tsukahara <seven00102@gmail.com>
2015-03-29 22:09:41 +0900

    created samplecontroller.phpe

---
+++ app/Controller/SampleController.php
@@ -0,0 +1,19 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class SampleController extends AppController {
+
+	public function index(){
+		$this-> autoRender = false;
+		$this-> redirect("./other/");
+	}
+	
+	public function other(){
+
+		$this-> autoRender = false;
+         echo "<html><head></head><body>";
+         echo "<h1>サンプルpage</h1>";
+         echo "</body></html>";
+
+	}
+}
